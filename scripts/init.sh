aa68003a2a23cbeaad9aa7f804dea8d129a4e515
Marko Vucen <marko_vucen@hotmail.com>
2019-07-18 14:28:35 +0200

    Refactor and use git repository for base install
    Move all installer script to github repository (`vukmark/vukmark-bin`) and use this repository when run `scripts/init.sh`.
    
    This way we can use bash script outside of this project.

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,5 +7,26 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 # Disable daily apt unattended updates.
 echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
 
-apt update
-apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y git
+
+# Make sure we are in home folder
+cd $HOME
+
+git clone https://github.com/vukmark/vukmark-bin.git
+
+cd $HOME/vukmark-bin/bin
+
+chmod +x ./*
+
+./setup_config.sh
+
+cd $HOME
+
+source .profile
+
+install_common_software.sh
+install_lamp.sh
+install_composer.sh
